8a0bb5c8e66c4b2bb05652934fb50871ced0ea4b
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2023-11-28 11:12:08 +0100

    Update index.js

--- functions/getNotes/index.js
+++ functions/getNotes/index.js
@@ -1,7 +1,28 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 
 exports.handler = async (event, context) => {
 
 
-    
+    const params = {
+        TableName: 'myNotesTable',
+        KeyConditionExpression: 'userId = :uid', 
+        ExpressionAttributeValues: {
+          ':uid': event.requestContext.authorizer.principalId
+        }
+    }
 }
+
+try {
+    const data = await dynamoDB.query(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Could not fetch notes.' })
+    };
+  }
