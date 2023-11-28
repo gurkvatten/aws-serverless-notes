558b8d0eeb35b85a846c6ba350250754f06155c1
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2023-11-28 11:25:02 +0100

    funcs

--- functions/patchNotes/index.js
+++ functions/patchNotes/index.js
@@ -1,7 +1,45 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 
 exports.handler = async (event, context) => {
+    const noteId = event.pathParameters.noteId; 
 
-
+    const { title, text } = JSON.parse(event.body); 
+  
     
-}
+    const userId = event.requestContext.authorizer.principalId; 
+  
+    const params = {
+      TableName: 'myNotesTable',
+      Key: {
+        userId: userId, 
+        id: noteId
+      },
+      UpdateExpression: 'SET #t = :title, #txt = :text, #mod = :modified',
+      ExpressionAttributeNames: {
+        '#t': 'title',
+        '#txt': 'text',
+        '#mod': 'modifiedAt'
+      },
+      ExpressionAttributeValues: {
+        ':title': title,
+        ':text': text,
+        ':modified': new Date().toISOString()
+      },
+      ReturnValues: 'ALL_NEW' 
+    };
+  
+    try {
+      const data = await dynamoDB.update(params).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data.Attributes)
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Could not update note.' })
+      };
+    }
+  };
--- functions/postNotes/index.js
+++ functions/postNotes/index.js
@@ -1,7 +1,35 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 
 exports.handler = async (event, context) => {
-
+    const { title, text } = JSON.parse(event.body); 
 
     
-}
+    const userId = event.requestContext.authorizer.principalId; 
+  
+    const params = {
+      TableName: 'myNotesTable',
+      Item: {
+        userId: userId, 
+        id: Math.random().toString(36).substring(2), 
+        title: title,
+        text: text,
+        createdAt: new Date().toISOString(),
+        modifiedAt: new Date().toISOString()
+      }
+    };
+  
+    try {
+      await dynamoDB.put(params).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Note created successfully.' })
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Could not create note.' })
+      };
+    }
+  };
